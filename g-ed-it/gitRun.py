2bbc21e803d4ee1429ef147720cb3175f4ac0a38
Matthieu Gautier <mgautier@fedoraproject.org>
2009-09-20 19:48:08 +0200

    fix lauching a git command

--- g-ed-it/gitRun.py
+++ g-ed-it/gitRun.py
@@ -5,10 +5,10 @@ import subprocess
 
 
 def gitRun(action, arguments, cwd):
-	command_line = 'git %s '%action
-	command_lines = [command_line]
-	command_lines.append(arguments)
-	print command_lines
-	command = subprocess.Popen(command_lines,shell=True,cwd=cwd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	command_line = 'git %s'%action
+	if isinstance(arguments, list):
+		arguments = " ".join(arguments)
+	command_line = command_line+' '+arguments
+	command = subprocess.Popen(command_line,shell=True,cwd=cwd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	(stdoutdata, stderrdata) = command.communicate()
 	return (command.returncode, stdoutdata, stderrdata)
